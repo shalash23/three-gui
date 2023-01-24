b5dba5ff69d74d6c07513b5e07cac5ebc4c5c7d1
shalash23 <shalashmohamed232@outlook.com>
2023-01-24 16:52:48 +0200

    lesson 14 start

--- three/main.js
+++ three/main.js
@@ -2,6 +2,9 @@ import * as THREE from "three";
 import gsap from "gsap";
 import * as dat from "dat.gui";
 import "./style.css";
+import typeFaceFont from "./node_modules/three/examples/fonts/helvetiker_regular.typeface.json";
+import { FontLoader } from "./node_modules/three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "./node_modules/three/examples/jsm/geometries/TextGeometry";
 
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls";
 
@@ -13,10 +16,74 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * GUI
+ * Loaders
+ */
+
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture1 = textureLoader.load("./textures/matcaps/1.png");
+const matcapTexture2 = textureLoader.load("./textures/matcaps/2.png");
+const matcapTexture3 = textureLoader.load("./textures/matcaps/3.png");
+const fontLoader = new FontLoader();
+const font = fontLoader.load(
+  "./node_modules/three/examples/fonts/helvetiker_regular.typeface.json",
+  (font) => {
+    const textGeometry = new TextGeometry(`Journey to The Edge of Space`, {
+      font: font,
+      size: 1,
+      height: 0.3,
+      curveSegments: 14,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.004,
+      bevelOffset: 0,
+      bevelSegments: 5,
+    });
+    const textMaterial = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture2,
+    });
+    const text = new THREE.Mesh(textGeometry, textMaterial);
+    // textGeometry.computeBoundingBox();
+
+    // textGeometry.translate(
+    //   -textGeometry.boundingBox.max.x * 0.5,
+    //   -textGeometry.boundingBox.max.y * 0.5,
+    //   -textGeometry.boundingBox.max.z * 0.5
+    // );
+    textGeometry.center();
+
+    text.position.x = 2;
+    text.position.y = 2;
+    text.position.z = 10;
+
+    scene.add(text);
+  }
+);
+
+for (let i = 0; i < 250; i++) {
+  const geometry = new THREE.TorusGeometry(0.3, 0.8, 16, 32);
+  const material = new THREE.MeshToonMaterial({});
+  const donut = new THREE.Mesh(geometry, material);
+  donut.position.set(
+    (Math.random() - 0.5) * 200,
+    (Math.random() - 0.5) * 200,
+    (Math.random() - 0.5) * 200
+  );
+  donut.rotation.x = Math.random() * Math.PI;
+  donut.rotation.y = Math.random() * Math.PI;
+  const scale = Math.random();
+  donut.scale.set(scale, scale, scale);
+  scene.add(donut);
+  console.timeEnd("donut");
+}
+
+/**
+ * GUI &  Helpers
  */
 
 const gui = new dat.GUI();
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
 /**
  * Parameters
@@ -50,20 +117,17 @@ const torus = new THREE.Mesh(
   material
 );
 
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(0.2, 0.5, 16, 5),
-  material
-);
-torus.position.set(3, 0, 0);
-plane.position.set(-2, 0, 0);
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 0.5, 16, 5), material);
+torus.position.set(-3, 0, 0);
+plane.position.set(1, 0, 0);
 scene.add(sphere, torus, plane);
 
 /**
  * Camera
  */
 
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
-camera.position.set(2, 3, 5);
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+camera.position.set(2, 3, 17);
 
 camera.lookAt(sphere.position);
 
